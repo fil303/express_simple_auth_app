70bc8c5903e0abec07d4693af62ecd20d286f640
fil303 <webfil303@gmail.com>
2023-07-24 22:14:17 +0600

    simple change

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,14 @@
 const express = require("express")
-var {
-    getRegisterPage,
-    proccessRegistation,
-    getLoginPage,
-    proccessLogin,
-    logout,
-} = require('../controllers/authController')
+var Auth = require('../controllers/authController')
 
 var Route = express.Router()
 
 Route.route('/register')
-    .get(getRegisterPage)
-    .post(proccessRegistation)
+    .get(Auth.getRegisterPage)
+    .post(Auth.proccessRegistation)
 Route.route('/login')
-    .get(getLoginPage)
-    .post(proccessLogin)
-Route.get("/logout", logout)
+    .get(Auth.getLoginPage)
+    .post(Auth.proccessLogin)
+Route.get("/logout", Auth.logout)
 
 module.exports = Route;
